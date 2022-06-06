fff96274664b86ca5aca83b84882feda611ed050
rkirk430 <r.kirk430@gmail.com>
2022-06-06 10:14:11 -0400

    localhost rendering

---
+++ server.js
@@ -0,0 +1,36 @@
+// Import Dependencies
+const express = require("express");
+const cors = require("cors");
+
+// Import JSON files
+const sentiment = require("./sentiment.json");
+const government = require("./government.json");
+
+// Create our app object
+const app = express();
+
+// set up middleware
+app.use(cors());
+
+//home route for testing our app
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// route for retrieving gov't trade data
+app.get("/government", (req, res) => {
+  // send gov't data via JSON
+  res.json(government);
+});
+
+// route for retrieving sentiment info
+app.get("/sentiment", (req, res) => {
+  // send projects via JSON
+  res.json(sentiment);
+});
+
+//declare a variable for our port number
+const PORT = process.env.PORT || 4000;
+
+// turn on the server listener
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
