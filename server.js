b0b0346b0059bd03ce11e868190ca0903efb3737
rkirk430 <r.kirk430@gmail.com>
2022-06-06 11:27:09 -0400

    Heroku Data JSON Rendering

--- server.js
+++ server.js
@@ -34,3 +34,10 @@ const PORT = process.env.PORT || 4000;
 
 // turn on the server listener
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+
+//Heroku Deployment//
+
+//== https://p4-altdata-backend.herokuapp.com/
+//== https://p4-altdata-backend.herokuapp.com/government
+//== https://p4-altdata-backend.herokuapp.com/sentiment
